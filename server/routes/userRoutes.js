e7920b640fa628e0e06219614a49f6e378ebbe80
Prince Ampofo <pla252@nyu.edu>
2021-08-12 01:20:10 +0000

    updated routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const routesFunction = require("./controllers/routeFunctions");
 
 // routes
-router.get("/", routesFunction.view);
-router.post("/", routesFunction.search);
-router.get("/addproject", routesFunction.addPage);
-router.post("/addproject", routesFunction.add);
-router.get("/editproject/:id", routesFunction.editPage);
-router.post("/editproject/:id", routesFunction.update);
-router.get("/viewproject/:id", routesFunction.viewOne);
-router.get("/:id", routesFunction.delete);
+router.get("/", routesFunction.view); // view landing page
+router.post("/", routesFunction.search); //search for project
+router.get("/addproject", routesFunction.addPage); // redirect to add project page
+router.post("/addproject", routesFunction.add); // add project
+router.get("/editproject/:id", routesFunction.editPage); // edit project page
+router.post("/editproject/:id", routesFunction.update); // update project
+router.get("/viewproject/:id", routesFunction.viewOne); // view a project
+router.get("/:id", routesFunction.delete); // delete a project
 
 module.exports = router;
